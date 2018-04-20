40a3c15ab2137eaa6fdd07c37af75be7bd7844d7
ShairaT <ayelenmaite2000@gmail.com>
2018-04-20 09:44:42 -0300

    Ejercicio 5??? falta probar el mainñññññ

--- Main_ejercicio5.py
+++ Main_ejercicio5.py
@@ -7,13 +7,27 @@ from Clase_album import Album
 
 
 discografica = Discografica()
-folie = Album()
-
-folie = input("album nuebooo: ")
-discografica.AgregarAlbum(folie)
+album = Album()
+cancion = Canciones()
+artista = Artista()
+autor = Autores()
+album.titulo = input("albun nuebooo xddd: ")
+discografica.AgregarAlbum(album)
 
 argentina = input("pais del artista mas influyente: ")
 discografica.ListaCancionesPais(argentina)
+
+cancion.titulo = input("Titulo de la cancion: ")
+album.AgregarCanciones(cancion)
+
+artista.nombre = input("Artista a a a a: ")
+autor.nombre = input("Autor: ")
+cancion.AgregarArtistas(artista)
+cancion.AgregarAutores(autor)
+
+
+
+
 
 
 
